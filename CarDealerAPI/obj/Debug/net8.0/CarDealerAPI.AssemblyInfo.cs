72c6735b84be0cb485bc2ec24853ba656fdbde69
FaustoPulido <fpulido@frsn.utn.edu.ar>
2025-07-09 13:19:02 -0300

    cambio 2343

--- CarDealerAPI/obj/Debug/net8.0/CarDealerAPI.AssemblyInfo.cs
+++ CarDealerAPI/obj/Debug/net8.0/CarDealerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarDealerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f2d08d1041dec93d25199f79e4fb6befa5baa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+085fcf3f876d90415eed753c0daf67030a3adb6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarDealerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarDealerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CarDealerAPI/obj/Release/net8.0/CarDealerAPI.AssemblyInfo.cs
+++ CarDealerAPI/obj/Release/net8.0/CarDealerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarDealerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b585e2d8da41dc25d17b18aa8d394ef4b4c4539d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+085fcf3f876d90415eed753c0daf67030a3adb6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarDealerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarDealerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
