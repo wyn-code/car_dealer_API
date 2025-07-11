1e9f1866743005527fda29bb87e7a17e9e7123b5
wyn-code <brunoo6.massocco@gmail.com>
2025-07-11 09:17:07 -0300

    pull de main

--- CarDealerAPI/obj/Debug/net8.0/CarDealerAPI.AssemblyInfo.cs
+++ CarDealerAPI/obj/Debug/net8.0/CarDealerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarDealerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee83e31bed4c7298219d3185deeeebd62c219149")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b27271dda55f231ba05651bf12c383d42a74536b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarDealerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarDealerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
